529bc09516ce9389408e36b20dd7a89664b7d623
Shay Amram <shay.teaching@gmail.com>
2024-10-26 16:34:01 +0000

    Fixed search button and box sizes. Bugfix: results now return even if the tab isn't on focus

--- src/app/main.py
+++ src/app/main.py
@@ -7,14 +7,14 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from graphrag.query.cli import run_local_search, run_global_search
-import uvicorn
 
 
 app = FastAPI()
 # templates = Jinja2Templates(directory="app/templates")
 templates = Jinja2Templates(directory="templates")
-# graphrag_root = Path("app/data/mafia_autotune")
-graphrag_root = Path("/workspaces/guidescanner/graphrag_tests/bg3_shadowheart_tuning")
+graphrag_root = Path("/workspaces/guidescanner/graphrag_tests/mafia_autotune")
+# graphrag_root = Path("/workspaces/guidescanner/graphrag_tests/bg3_shadowheart_tuning")
+
 graphrag_data = graphrag_root / "output"
 config_path = graphrag_root / "settings.yaml"
 community_level = 2
@@ -28,33 +28,21 @@ async def run_in_thread(func, *args):
     return await loop.run_in_executor(executor, func, *args)
 
 
-# def post_query(config_filepath, data_dir, root_dir, community_level, response_type, streaming, query):
-#     result = run_global_search(config_filepath, data_dir, root_dir, community_level, response_type, streaming, query)
-#     return result
-
 async def post_query(config_filepath, data_dir, root_dir, community_level, response_type, streaming, query):
     result = await run_in_thread(run_global_search, config_filepath, data_dir, root_dir, community_level, response_type, streaming, query)
     return result
 
-
-# @app.get("/", response_class=HTMLResponse)
-# async def home(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-# @app.get("/search", response_class=HTMLResponse)
-# async def search(request: Request, query: str):
-#     results = await post_query(config_path, graphrag_data, graphrag_root, community_level, response_type, streaming, query)  # Perform search based on query
-#     return templates.TemplateResponse("results.html", {"request": request, "results": results})
-
 @app.get("/search")
 async def search(query: str):
     results = await post_query(config_path, graphrag_data, graphrag_root, community_level, response_type, streaming, query)
     return {"results": results}
+
 
 if __name__ == "__main__":
     import uvicorn
